50a1648f4c0f691f82e70ed3c7d93f69adde5d7c
Piyush014 <109540904+Piyush014@users.noreply.github.com>
2024-07-05 14:08:42 +0530

    Toast demo

---
+++ 17_Toast_Demo_App/app/src/main/java/com/pyush/a17_toast_demo_app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.pyush.a17_toast_demo_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Successfully Shown Toast", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
